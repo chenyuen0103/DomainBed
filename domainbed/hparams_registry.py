f547a08b0ccf17da768ba4a58ecb7dd845fd0735
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-04-09 16:47:28 -0500

    add ViTS

--- domainbed/hparams_registry.py
+++ domainbed/hparams_registry.py
@@ -147,7 +147,7 @@ def _hparams(algorithm, dataset, random_seed):
         _hparam('lr', 0.03, lambda r: 10 ** r.uniform(-2, -1))
         _hparam('weight_decay', 0.0005, lambda r: 10 ** r.uniform(-6, -2))
         _hparam('batch_size', 64, lambda r: int(2 ** r.uniform(5, 8)))
-        _hparam('img_size', 384, lambda r: 384)
+        _hparam('img_size', 224, lambda r: 384)
         _hparam('warmup_steps', 100, lambda r: 100)
         _hparam('num_steps', 700, lambda r: 700)
 
