acc4b3ed303905bad8721b5864e1eb7120511cda
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-04-30 14:15:19 -0500

    update hessian norm computation

--- domainbed/algorithms.py
+++ domainbed/algorithms.py
@@ -244,7 +244,7 @@ class HessianAlignment(ERM):
         grad_loss = p - y_onehot
         grad_w = torch.matmul(grad_loss.T, x_flattened) / x.size(0)
         # grad_w /= (grad_w.shape[0] * grad_w.shape[1]) ** 0.25
-        grad_w /= dC ** 0.25
+        grad_w /= dC ** 0.5
 
         return grad_w
 
