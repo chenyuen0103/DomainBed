f08c263f299665b0212240684836d0a082c78595
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-04-30 20:38:50 -0500

    add file name

--- domainbed/algorithms.py
+++ domainbed/algorithms.py
@@ -192,7 +192,7 @@ class HessianAlignment(ERM):
         # Combine the probabilities with the outer product of x
         H2 = torch.zeros(dC, dC, device=x.device)
 
-        mini_batch_size = 6
+        mini_batch_size = 4
         for i in range(0, batch_size, mini_batch_size):
             try:
                 H2 += torch.einsum('bkl, bij -> bklij', p_off_diag[i: i + mini_batch_size], X_outer[i: i + mini_batch_size]).sum(0).reshape(dC, dC)
