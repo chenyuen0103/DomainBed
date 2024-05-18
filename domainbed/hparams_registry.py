f4165209637573a5dab897f82782445c6b12f55c
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-05-17 23:56:20 -0500

    1000, 5000

--- domainbed/hparams_registry.py
+++ domainbed/hparams_registry.py
@@ -153,7 +153,7 @@ def _hparams(algorithm, dataset, random_seed, model_type='ViT-S'):
         _hparam('grad_alpha', 1000, lambda r: 10 ** r.uniform(1, 3))
         _hparam('hess_beta', 500, lambda r: 10 ** r.uniform(2, 4))
         # _hparam('penalty_anneal_iters', 2500, lambda r: int(r.uniform(0., 5000.)))
-        _hparam('penalty_anneal_iters', 2500, lambda r: int(np.random.choice([1500, 2500, 3500, 4500])))
+        _hparam('penalty_anneal_iters', 500, lambda r: int(np.random.choice([500, 1500, 2500, 3500, 4500])))
 
         # _hparam('grad_alpha', 1000, lambda r: 1000)
         # _hparam('hess_beta', 1000, lambda r: 1000)
