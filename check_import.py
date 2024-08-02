6dbfc9ae5cfa4396589ee39435e6f0129b638c91
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-08-01 18:06:59 -0700

    add back anneal

--- check_import.py
+++
@@ -1,8 +0,0 @@
-import os
-print("PYTHONPATH:", os.environ.get("PYTHONPATH"), flush=True)
-print("PATH:", os.environ.get("PATH"), flush=True)
-import domainbed.scripts.sweep
-print("DomainBed module imported successfully", flush=True)
-import sys
-print("sys.path:", sys.path, flush=True)
-sys.exit(0)
