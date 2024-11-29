593572d7443f764f84f7d5d6403b3e145dad81e9
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-11-29 08:42:40 -0600

    20241129

--- run_dann.sh
+++
@@ -1,1 +0,0 @@
-/home/yuenchen/anaconda3/envs/isr/bin/python -m domainbed.scripts.sweep delete_incomplete       --data_dir=./domainbed/data/    --skip_confirmation --output_dir=./domainbed/results_dann       --command_launcher multi_gpu      --algorithms DANN       --datasets ColoredMNIST RotatedMNIST      --single_test_envs       --n_hparams 5       --n_trials 3
--- run_resnet.sh
+++
@@ -1,1 +0,0 @@
-/home/yuenchen/anaconda3/envs/isr/bin/python -m domainbed.scripts.sweep launch       --data_dir=./domainbed/data/    --skip_confirmation --output_dir=./domainbed/results_resnet       --command_launcher multi_gpu       --algorithms CMA       --datasets PACS       --single_test_envs       --n_hparams 5       --n_trials 3
